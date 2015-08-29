9d8c121983f036e203a9dc4f30e6cf6334a56592
Matt Steedman <steedman87@gmail.com>
2015-08-29 19:39:29 -0400

    Rewrite orthogonal function
    Now accepts "forwards", "backwards", and "sideways" while checking piece
    color.

--- src/js/chess.js
+++ src/js/chess.js
@@ -1,13 +1,15 @@
 var R        = require('ramda');
 var Errors   = require('./errors');
 var Types    = require('./lib/types');
+// var {Board, Piece, Position} = require('./Types');
 var Board    = require('./types/Board');
 var Piece    = require('./types/Piece');
 var Position = require('./types/Position');
 
-//  orthogonal :: (Maybe String, Either String | Number, Board, Position) -> [Position]
-var orthogonal = R.curry(function(direction, distance, board, position) {
+//  orthogonal :: (Maybe String, Either String | Number, Board, Piece) -> [Position]
+var orthogonal = R.curry(function(direction, distance, board, piece) {
   distance = distance === 'n' ? board.size - 1 : parseInt(distance);
+  var position = piece.position
 
   var getPosition = R.curry(function(axis, i) {
     return {
@@ -21,34 +23,48 @@ var orthogonal = R.curry(function(direction, distance, board, position) {
   // hence if parameter is falsey it will return falsey.
   return R.filter(R.identity, R.flatten(
     R.map(function(axis) {
-      var min = Math.max(position[axis] - distance, 0)
-      var max = Math.min(position[axis] + distance + 1, board.size)
-      return R.map(function(i) {
-        var inBetween = i < position[axis]
-                      ? R.range(i, position[axis])
-                      : R.range(position[axis] + 1, i + 1);
-
-        var blockingPieces = R.any(
-          R.compose(
-            getPieceAtPosition(board),
-            getPosition(axis)
-          ), inBetween);
-
-        // TODO: moveType check
-        if (blockingPieces) {
-          // TODO: check if piece is opposite color, add to captures
-          return false; // Gets filtered out.
-        } else {
-          return getPosition(axis, i);
+      if ( (direction === 'forwards' || direction === 'backwards') && axis === 'x') {
+        return false;
+      } else if (direction === 'sideways' && axis === 'y') {
+        return false
+      } else {
+        // direction === null || direction === 'sideways'
+        var min = Math.max(position[axis] - distance, 0); // or position[axis] if forwards
+        var max = Math.min(position[axis] + distance + 1, board.size); // or position[axis] if backwards
+        if ( (direction === 'forwards' && piece.color === 'white')
+          || (direction === 'backwards' && piece.color === 'black') ) {
+          var min = position[axis];
+        } else if ( (direction === 'backwards' && piece.color === 'white')
+                 || (direction === 'forwards' && piece.color === 'black') ) {
+          var max = position[axis] + 1;
         }
-      }, R.concat(R.range(min, position[axis]), R.range(position[axis] + 1, max)));
+        return R.map(function(i) {
+          var inBetween = i < position[axis]
+                        ? R.range(i, position[axis])
+                        : R.range(position[axis] + 1, i + 1);
+
+          var blockingPieces = R.any(
+            R.compose(
+              getPieceAtPosition(board),
+              getPosition(axis)
+            ), inBetween);
+
+          // TODO: moveType check
+          if (blockingPieces) {
+            // TODO: check if piece is opposite color, add to captures
+            return false; // Gets filtered out.
+          } else {
+            return getPosition(axis, i);
+          }
+        }, R.concat(R.range(min, position[axis]), R.range(position[axis] + 1, max)));
+      }
     }, ['x', 'y'])
   ));
 });
 
 var directions = {
-  '+': orthogonal(null),
-  //'+': R.converge(R.concat, orthogonal('forwards'),orthogonal('backwards'),orthogonal('sideways')),
+  //'+': R.converge(R.concat, orthogonal('sideways'), orthogonal('backwards'), orthogonal('forwards')), // This won't work because R.concat is not variadic.
+  '+': R.converge(R.concat, R.converge(R.concat, orthogonal('sideways'), orthogonal('backwards')), orthogonal('forwards')),
   '>': orthogonal('forwards'),
   '<': orthogonal('backwards'),
   '=': orthogonal('sideways'),
@@ -71,7 +87,7 @@ var legalPosition = R.curry(function(board, position) {
 var getMoves = R.curry(function(board, piece) {
   //R.compose(R.uniq, R.flatten, R.map)
   return R.uniq(R.flatten(R.map(function(p) {
-    return directions[p.direction](p.distance, board, piece.position);
+    return directions[p.direction](p.distance, board, piece);
   }, piece.parlett)));
 });
 
